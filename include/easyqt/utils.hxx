34198b8b8ac34dbd50056e397d32109c0f413b1b
TheFGFSEagle <thefgfseagle@gmail.com>
2026-09-04 16:26:47 +0200

    Add a portable way to get the name of a class

--- include/easyqt/utils.hxx
+++ include/easyqt/utils.hxx
@@ -1,5 +1,5 @@
-#ifndef FGAUGE_UTILS_HXX
-#define FGAUGE_UTILS_HXX
+#ifndef EASYQT_UTILS_HXX
+#define EASYQT_UTILS_HXX
 
 #include <ctime>
 #include <string>
@@ -10,6 +10,31 @@ namespace easyqt {
 	std::string getResourcePath(std::string path);
 	std::string strftime(std::time_t time, const std::string& format);
 	std::string strftime(std::time_t time, const char* format);
+
+	template <typename T>
+	constexpr std::string typeName() {
+#if defined(__clang__)
+		constexpr auto prefix = std::string_view{"[T = "};
+		constexpr auto suffix = "]";
+		constexpr auto function = std::string_view{__PRETTY_FUNCTION__};
+#elif defined(__GNUC__)
+		constexpr auto prefix = std::string_view{"with T = "};
+		constexpr auto suffix = "; ";
+		constexpr auto function = std::string_view{__PRETTY_FUNCTION__};
+#elif defined(_MSC_VER)
+		constexpr auto prefix = std::string_view{"get_type_name<"};
+		constexpr auto suffix = ">(void)";
+		constexpr auto function = std::string_view{__FUNCSIG__};
+#else
+# error Unsupported compiler
+#endif
+
+		const auto start = function.find(prefix) + prefix.size();
+		const auto end = function.find(suffix);
+		const auto size = end - start;
+
+		return std::string(function.substr(start, size));
+	}
 }
 
 namespace std {
